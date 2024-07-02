3972358772fd983fc66601b5284042e1c04338fd
Kabelo <masolakabelo@gmail.com>
2024-07-02 16:29:55 +0200

    Initial commit for the fraud detection service

---
+++ src/main/java/za/co/ditorosystems/hydra/fraudservice/demo/FraudDetectionProcessorApplication.java
@@ -0,0 +1,13 @@
+package za.co.ditorosystems.hydra.fraudservice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectionProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudDetectionProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/ditorosystems/hydra/fraudservice/demo/FraudDetectionProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.ditorosystems.hydra.fraudservice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
